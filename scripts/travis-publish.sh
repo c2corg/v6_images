eb4bc94e4b5b57f0de66ea5b77b8709abbf86c6b
Marc Fournier <marc.fournier@camptocamp.com>
2016-06-25 01:00:49 +0200

    fix bashisms

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -4,13 +4,13 @@ REPO="c2corg/v6_images"
 
 docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo "Deploying image to docker hub for master (latest)"
   docker push "${REPO}:latest"
-elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
   docker push "${REPO}:${TRAVIS_TAG}"
-elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
   docker push "${REPO}:${TRAVIS_BRANCH}"
 else
