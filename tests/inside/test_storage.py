ba44fa672c60953109f5c09f46578099cf9b5b6f
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-06-24 16:19:37 +0200

    Add publish core methods

--- tests/inside/test_storage.py
+++ tests/inside/test_storage.py
@@ -3,7 +3,7 @@ import shutil
 import tempfile
 from tests import utils
 
-from c2corg_images.storage import send_local, send_s3
+from c2corg_images.storage import send_local, send_s3, publish_s3
 
 
 def test_send_local():
@@ -18,10 +18,12 @@ def test_send_local():
 
 
 @utils.skipIfTravis
-def test_send_s3():
+def test_s3():
     with tempfile.NamedTemporaryFile(suffix='.png') as tmp_file:
         shutil.copyfile('tests/piano.png', tmp_file.name)
 
         dirname = os.path.dirname(tmp_file.name)
         filename = os.path.basename(tmp_file.name)
         send_s3(dirname, filename)
+
+        publish_s3(filename)
