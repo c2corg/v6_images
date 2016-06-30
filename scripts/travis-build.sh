6138a904d829d907ae16834d05d8d5b9ca41657c
Marc Fournier <marc.fournier@camptocamp.org>
2016-06-30 21:29:36 +0200

    travis-*sh: avoid building docker images from pull-requests & improve console output

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -2,14 +2,19 @@
 
 REPO="c2corg/v6_images"
 
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Not building docker images out of Pull Requests"
+  exit 0
+fi
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo "Building image with tag latest"
+  echo "Building image '${REPO}:latest'"
   docker build -t "${REPO}:latest" .
-elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Building image with tag ${TRAVIS_TAG}"
+elif [ ! -z "$TRAVIS_TAG" ]; then
+  echo "Building image '${REPO}:${TRAVIS_TAG}'"
   docker build -t "${REPO}:${TRAVIS_TAG}" .
-elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo "Building image with tag ${TRAVIS_BRANCH}"
+elif [ ! -z "$TRAVIS_BRANCH" ]; then
+  echo "Building image '${REPO}:${TRAVIS_BRANCH}'"
   docker build -t "${REPO}:${TRAVIS_BRANCH}" .
 else
   echo "Don't know how to build image"
