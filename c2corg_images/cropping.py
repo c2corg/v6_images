7410236635dc004ab4dad0b77b9094e95ae87fba
Roman Zoller <roman.zoller@camptocamp.com>
2017-04-26 16:46:10 +0200

    Add cropping functionality
    - Add 'crop' option to /publish which allows to crop a photo before
      publishing it
    - Add /recrop route that allows to
      - re-crop a photo if 'crop' option is present
      - disable cropping and re-generate thumbnails from original if
        'crop' option is absent
      In both cases, the result is published under a new name which is
      returned as 'filename'

---
+++ c2corg_images/cropping.py
@@ -0,0 +1,14 @@
+import os
+from c2corg_images.convert import transform
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def create_cropped_image(path: str, filename: str, crop_options: str):
+    full_path = os.path.join(path, filename)
+    log.info('Cropping image %s', full_path)
+    crop_config = ['-crop', crop_options]
+    transform(full_path,
+              full_path,
+              crop_config)
